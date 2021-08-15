f61fb79ae84d4f5ab158434c7ea75298e9340945
Craig <craigjope@gmail.com>
2021-08-14 21:49:42 -0600

    commit tree 38500d3db7847e0c3d66faa347162fcb6cd37250

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase()+string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^a-zA-Z0-9 '-]/g, '')
+    
+  }
+  static titleize(string){
+    lowercase = ["the","a","an","but","of","and","for","at","by","from"]
+    newString = []
+    splitString = string.split(" ")
+  //need to loop through the splitString and identify any words that are direct matches to the array (do I loop through that too?) then return the array or something. I don't know. I'm tired.
+    
+    return newString.toUpperCase()
+  }
 }
